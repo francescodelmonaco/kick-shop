3ebaf5a27a06789ed71e876224c41b2c156a5393
413f10r <ale-fioretti@live.it>
2025-03-28 17:49:31 +0100

    ADD: DefLayout, HomePage, NavBar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,22 @@
 
-
-// function App() {
-
-
-//   return (
-//     <>
+import DefaultLayout from "./layouts/DefaultLayout"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from "../src/pages/HomePage"
+function App() {
 
 
-//     </>
-//   )
-// }
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route Component={DefaultLayout}>
+                        <Route path="/" Component={HomePage} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
 
-// export default App
+        </>
+    )
+}
+
+export default App
