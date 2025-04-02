9d315f1143d01a25132cf44081ca5050350e0699
413f10r <ale-fioretti@live.it>
2025-04-02 10:36:11 +0200

    add: implement summer products fetching and display on SummerPage

--- Frontend/src/pages/SummerPage.jsx
+++ Frontend/src/pages/SummerPage.jsx
@@ -1,8 +1,54 @@
+
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import ProductCard from '../components/ProductCard';
+
 export default function SummerPage() {
+
+    // CHIAMATA PRODOTTI UOMO
+    const [summerProducts, setSummerProducts] = useState([]);
+
+    // fetch per prodotti
+    const fetchProducts = () => {
+        // console.log('Fetching products...')
+
+        axios
+            .get('http://localhost:3000/products')
+            .then((res) => {
+                setSummerProducts(res.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+
+    // rendering prodotti in html
+    const renderSummerProducts = () => {
+        return summerProducts
+            .filter((summerProduct) => summerProduct.season === "estate") // Filtra per genere
+            .map((summerProduct) => (
+                <div className="col g-3 pb-3" key={summerProduct.id}>
+                    <ProductCard product={summerProduct} />
+                </div>
+            ));
+    };
+
+    // invocazione chiamata al caricamento del componente in pagina
+    useEffect(fetchProducts, []);
+
     return (
         <>
-            <h1 className="text-center py-3">Summer Collection</h1>
+            <h1 className="text-center py-3">Collezione Estate</h1>
 
+            <figure>
+                <img src="/src/assets/img/hero-section-2-man.webp" alt="Hero image 3" className="w-100 hero-border" />
+            </figure>
+
+            <div className="px-5">
+                <div className="row row-cols-lg-4 mb-5">
+                    {renderSummerProducts()}
+                </div>
+            </div>
         </>
     )
 }
